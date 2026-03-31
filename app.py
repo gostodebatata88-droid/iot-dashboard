ff00e53f50a619eb3887b934635dea97f2563948
gostodebatata88-droid <gostodebatata88@gmail.com>
2026-03-31 20:00:23 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+url = "LINK_CSV_AQUI"
+df = pd.read_csv(url)
+st.title("Monitoramento IoT")
+st.line_chart(df[['temperatura', 'umidade']])
+st.dataframe(df)
